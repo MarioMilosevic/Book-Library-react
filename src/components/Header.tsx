f166c54e0b7f40e534d8b09afc2c37e77d1c47e9
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-14 13:20:18 +0100

    Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="pt-20 text-center">
+      <h1 className="text-5xl pb-8 font-semibold">Book Library</h1>
+      <h2 className="text-3xl pb-8">Boost Your Book Collection: Just Tap Below to Add and Watch Your Library Shine!</h2>
+      <button className="bg-sky-500 text-sky-50 duration-300 rounded-full px-6 py-3 text-2xl hover:scale-110">New Book</button>
+    </div>
+  )
+}
+
+export default Header
